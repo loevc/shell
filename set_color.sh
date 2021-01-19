bb5db8d2623ce68447fb4147259150a472b17e98
loevc <33386034+loevc@users.noreply.github.com>
2021-01-19 19:02:54 +0800

    Create set_color.sh

---
+++ set_color.sh
@@ -0,0 +1,9 @@
+#! /usr/bash
+# Description: centos7
+# Author : loevc
+# Time : 2021.1.19
+
+
+
+# set the tip command as the i like
+export PS1="\[\e[35;1m\][\[\e[33;1m\]\u\[\e[31;1m\]@\[\e[33;1m\]\h \[\e[36;1m\]\w\[\e[35;1m\]]\[\e[32;1m\]\\$ \[\e[0m\]"
